af538a6d81d2032d1b0d5a575e7b92e585dd872c
Ville Virnala <ville.virnala@gmail.com>
2022-02-17 23:49:54 +0200

    local takas

--- vue.config.js
+++ vue.config.js
@@ -2,22 +2,3 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true
 })
-
-const path = require('path');
-
-module.exports = {
-    devServer: {
-        proxy: {
-            '/api/parkano': {
-                target: 'http://localhost:8080/'
-            },
-            '/api/seinajoki': {
-                target: 'http://localhost:8080/'
-            },
-            '/api/ilmajoki': {
-                target: 'http://localhost:8080/'
-            }
-
-        }
-    }
-};
