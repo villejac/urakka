5fde7012287cfec8d9df492d8d208725806ddf05
Ville Virnala <ville.virnala@gmail.com>
2022-02-17 23:43:53 +0200

    local

--- vue.config.js
+++ vue.config.js
@@ -3,3 +3,21 @@ module.exports = defineConfig({
   transpileDependencies: true
 })
 
+const path = require('path');
+
+module.exports = {
+    devServer: {
+        proxy: {
+            '/api/parkano': {
+                target: 'http://localhost:8080/'
+            },
+            '/api/seinajoki': {
+                target: 'http://localhost:8080/'
+            },
+            '/api/ilmajoki': {
+                target: 'http://localhost:8080/'
+            }
+
+        }
+    }
+};
